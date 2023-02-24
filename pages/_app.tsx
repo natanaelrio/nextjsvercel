efc348a36b55f79cc76ffc96436b30075d13310f
Natanael Rio Wijaya <gafterlife@gmail.com>
2023-02-25 06:28:21 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import * as React from 'react';
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+}
+
+
+declare namespace JSX {
+  interface IntrinsicElements {
+      form: any
+  }
+}
+
+declare global {
+  namespace JSX {
+      interface IntrinsicElements {
+          'my-html-custom-tag': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      }
+  }
 }
